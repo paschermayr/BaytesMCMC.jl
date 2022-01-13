5f3a0568c65c387881c81776a8a591eae0bc6b11
Patrick Aschermayr <p.aschermayr@gmail.com>
2022-01-13 17:01:04 +0000

    Update Kernels.jl

--- src/Kernels/Kernels.jl
+++ src/Kernels/Kernels.jl
@@ -3,7 +3,6 @@ Working Examples for this module
 "
 ############################################################################################
 #Import
-
 include("utility/utility.jl")
 include("Metropolis/Metropolis.jl")
 include("MALA/MALA.jl")
