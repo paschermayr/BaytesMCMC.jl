53f969bf613c8cc3a885b0bf3a2b688ce96821f1
Patrick Aschermayr <p.aschermayr@gmail.com>
2022-01-13 17:22:31 +0000

    Update Kernels.jl

--- src/Kernels/Kernels.jl
+++ src/Kernels/Kernels.jl
@@ -6,8 +6,8 @@ Working Examples for this module
 #include("utility/utility.jl")
 include("Metropolis/Metropolis.jl")
 include("MALA/MALA.jl")
-include("HMC/HMC.jl")
-include("NUTS/NUTS.jl")
+#include("HMC/HMC.jl")
+#include("NUTS/NUTS.jl")
 
 ############################################################################################
 #Export ~ All MCMC algorithm need to be dispatched on the following functions:
